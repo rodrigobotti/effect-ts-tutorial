b3ba4e28c58e005307c69e980d16da70fdfa73a0
Rodrigo Botti <rodrigo.botti@gmail.com>
2025-09-07 11:13:06 -0300

    Add meaningful names to programs

--- src/Program.ts
+++ src/Program.ts
@@ -13,12 +13,12 @@ import { programAsync } from "./tutorials/3-async-1.js"
 import { prgramAsyncWithCleanup } from "./tutorials/3-async-2.js"
 import { prgramAsyncWithInterruption } from "./tutorials/3-async-3.js"
 import { programSuspend } from "./tutorials/4-suspend-1.js"
-import { prgramSuspendRecursive } from "./tutorials/4-suspend-2.js"
+import { prgramSuspendRecursive as programSuspendRecursive } from "./tutorials/4-suspend-2.js"
 import { programEither, programOption } from "./tutorials/5-option-either.js"
 import { programCollectEithers, programCollectionOptions, programCollectStruct } from "./tutorials/6-collecting.js"
 import { programTracing } from "./tutorials/7-tracing-1.js"
 import { programTracingOtel } from "./tutorials/7-tracing-2.js"
-import { programCatchDefect, programExit } from "./tutorials/8-defects-2.js"
+import { programCatchDefect, programCatchSomeDefect, programExit } from "./tutorials/8-defects-2.js"
 import {
   programFirstSuccessOf,
   programOrElse,
@@ -34,7 +34,7 @@ const runProgram = <A, E>(
   delay: DurationInput = "500 milli",
 ): Effect.Effect<void, never, never> =>
   pipe(
-    Console.log(`${SEPARATOR}\nRunning ${name}`),
+    Console.log(`${SEPARATOR}\nRunning Program ${name}`),
     Effect.andThen(program),
     Effect.andThen((v) => Console.log(`\nProgram ${name} succeeded${v !== undefined ? ` with ${v} !` : "!"}`)),
     Effect.catchAll((e) => Console.log(`\nProgram ${name} failed with ${e}`)),
@@ -43,55 +43,56 @@ const runProgram = <A, E>(
 
 const programs = {
   "1": [
-    runProgram("program 1", program_1),
+    runProgram("screwing around", program_1),
   ],
   "2": [
-    runProgram("program 2", programSync("a")),
+    runProgram("sync", programSync("a")),
   ],
   "3": [
     runProgram(
-      "program 3.1",
+      "async",
       programAsync({
         url: "https://google.com",
         path: "./LICENSE",
       }),
     ),
-    runProgram("Program 3.2", prgramAsyncWithCleanup),
-    runProgram("Program 3.3", prgramAsyncWithInterruption),
+    runProgram("async with clean up", prgramAsyncWithCleanup),
+    runProgram("async with interruption", prgramAsyncWithInterruption),
   ],
   "4": [
-    runProgram("Program 4.1", programSuspend),
-    runProgram("Program 4.2", prgramSuspendRecursive(32)),
+    runProgram("suspend", programSuspend),
+    runProgram("suspend with recursion", programSuspendRecursive(32)),
   ],
   "5": [
-    runProgram("Program 5", programOption),
-    runProgram("Program 5", programEither),
+    runProgram("effect->option", programOption),
+    runProgram("effect->either", programEither),
   ],
   "6": [
-    runProgram("Program 6", programCollectStruct),
-    runProgram("Program 6", programCollectEithers),
-    runProgram("Program 6", programCollectionOptions),
+    runProgram("collect as a struct", programCollectStruct),
+    runProgram("collect as list of eithers", programCollectEithers),
+    runProgram("collect with list of options", programCollectionOptions),
   ],
   "7": [
-    runProgram("Program 7.1", programTracing(10)),
-    runProgram("Program 7.2", programTracingOtel),
+    runProgram("tracing", programTracing(10)),
+    runProgram("tracing with open telemetry", programTracingOtel),
   ],
   "8": [
-    runProgram("Program 8.2", programExit),
-    runProgram("Program 8.2", programCatchDefect),
+    runProgram("handling Effect.exit", programExit),
+    runProgram("catching all defects", programCatchDefect),
+    runProgram("catching some defects", programCatchSomeDefect),
   ],
   "9": [
-    runProgram("Program 9", programOrElse()),
-    runProgram("Program 9", programOrElseFail),
-    runProgram("Program 9", programOrElseSucceed),
-    runProgram("Program 9", programFirstSuccessOf()),
+    runProgram("Effect.orElse", programOrElse()),
+    runProgram("Effect.orElseFail", programOrElseFail),
+    runProgram("Effect.orElseSucceed", programOrElseSucceed),
+    runProgram("fallback until first success", programFirstSuccessOf()),
   ],
   "10": [
-    runProgram("Program 10", programMatch),
-    runProgram("Program 10", programIgnore),
-    runProgram("Program 10", programMatchEffect),
-    runProgram("Program 10", programMatchCause),
-    runProgram("Program 10", programMatchCauseEffect),
+    runProgram("matching effects", programMatch),
+    runProgram("ignoring effect results", programIgnore),
+    runProgram("matching effects with effects", programMatchEffect),
+    runProgram("matching cause", programMatchCause),
+    runProgram("matching cause with effects", programMatchCauseEffect),
   ],
 } as const
 
